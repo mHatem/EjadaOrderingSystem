9353fd9ce64ad7f81e25ba5477989b8a94b67e1b
mohamed mamdouh <mm95mido@gmail.com>
2016-08-10 15:36:08 +0200

    adding place_ID to order

--- src/com/code/ui/order.java
+++ src/com/code/ui/order.java
@@ -3,18 +3,26 @@ package com.code.ui;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.view.facelets.FaceletContext;
 
+import com.code.dal.orm.Order;
 import com.code.dal.orm.OrderView;
 import com.code.dal.orm.Place;
 import com.code.services.orderService;
 
+import oracle.jdbc.util.Login;
+
 @SuppressWarnings("serial")
 @ManagedBean(name = "order")
 // don't forget to change it
-@ViewScoped
+@SessionScoped
 public class order implements Serializable {
 	private long id;
 	private String name;
@@ -24,30 +32,50 @@ public class order implements Serializable {
 	private Date date;
 	private float totalPrice;
 	private List<OrderView> orders;
-	private List<Place> places;
+	private String selectedPlace;
 	
+	
+	public long extractPlaceID(String n)
+	{
+		PlaceBean bean= new PlaceBean();
+		for(Place p : bean.getPlaces())
+		{
+		 if(p.getName() != null && p.getName().equals(n))
+		 {
+			 return p.getId();
+		 }
+		}
+		return 0;
+	}
 	public void displayAllOrders()
 	{
 		setOrders(orderService.getALL());
 	}
 
-	public void add()
+	public String add()
 	{
-		orders.add(null);
+		Map<String, Object> sessionMap=FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+		Date dat =new Date();
+		Order order = new Order();
+		order.setName(this.name);
+		order.setPlaceID(extractPlaceID(selectedPlace));
+		order.setStatus(this.status);
+		order.setOwnerID(sessionMap.get(Login.SESSION_KEY_USER_ID));
+		order.setDate(dat);
+		orderService.insert(order);
+		return null;
 	}
 	public order()
 	{
 		displayAllOrders();
 	}
 	
-	public List<Place> getPlaces() {
-		return places;
+	public String getSelectedPlace() {
+		return selectedPlace;
 	}
-
-	public void setPlaces(List<Place> places) {
-		this.places = places;
+	public void setSelectedPlace(String selectedPlace) {
+		this.selectedPlace = selectedPlace;
 	}
-
 	public long getOwnerID() {
 		return ownerID;
 	}
